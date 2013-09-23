bf201a937eb092d00c7f68fa7ccdaf4a5720bf28
Alethea Butler <alethea@aletheabutler.com>
2013-09-23 16:43:35 -0400

    Added client

---
+++ tinychat/client.py
@@ -0,0 +1,35 @@
+# 9/23/2013
+# Alethea Butler
+
+import socket
+
+
+class Client:
+    def __init__(self, server_addr=("127.0.0.1", 5280)):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_addr = server_addr
+        self.running = True
+
+    def run(self):
+        while self.running:
+            try:
+                message = bytes(input("Message? "), "UTF-8")
+            except EOFError:
+                self.running = False
+                break
+            if message:
+                sent = self.sock.sendto(message, self.server_addr)
+                print("{0} bytes sent".format(sent))
+            else:
+                print("no message sent")
+
+    def __del__(self):
+        self.running = False
+        try:
+            self.sock.close()
+        except socket.error as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    Client().run()
